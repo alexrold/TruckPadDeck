fbd4b8bb32fa88312a95381b14207e36bcdac9a5
Ronald A Betancourt F <RonaldBetancourt.dev@gmail.com>
2026-03-18 12:23:07 -0400

    chore: setup nativewind v4, rename app to truckpaddeck and configure path      aliases

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { ThemedText } from '@/components/themed/ThemedText';
-import { ThemedView } from '@/components/themed/ThemedView';
+import {ThemedText} from '@/components/themed/ThemedText';
+import {ThemedView} from '@/components/themed/ThemedView';
 import React from 'react';
-
 
 const HomeScreen = () => {
   return (
-    <ThemedView>
-      <ThemedText>HomTe Screen</ThemedText>
+    <ThemedView className="mt-20 flex-1 mx-4 ">
+      <ThemedText className="font-bold text-xl text-blue-800 ">
+        HomTe Screen
+      </ThemedText>
     </ThemedView>
   );
 };
