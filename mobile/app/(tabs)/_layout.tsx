d1dc9bee560f2e338aa70d778bf10d0f6e554926
Ronald A Betancourt F <RonaldBetancourt.dev@gmail.com>
2026-03-18 20:17:21 -0400

    ui(mobile): hide default tab bar and header to allow custom dashboard UI

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,32 +1,18 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {Tabs} from 'expo-router';
 import React from 'react';
 
-import Colors from '@/constants/Colors';
-import {useColorScheme} from '@/hooks/useColorScheme';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{marginBottom: -3}} {...props} />;
-}
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,      // Oculta el Header por defecto
+        tabBarStyle: { display: 'none' }, // Oculta la barra de pestañas (Tabs)
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({color}) => <TabBarIcon name="home" color={color} />,
+          title: 'Dashboard',
         }}
       />
     </Tabs>
