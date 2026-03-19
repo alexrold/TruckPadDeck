543e9c053a11b7de97eb63fba2c8f9f0f6ff262d
Ronald A Betancourt F <RonaldBetancourt.dev@gmail.com>
2026-03-19 16:45:24 -0400

    refactor(ui): update 404 screen with themed components and tailwind

--- mobile/app/+not-found.tsx
+++ mobile/app/+not-found.tsx
@@ -1,40 +1,24 @@
 import {ThemedText, ThemedView} from '@/components/themed';
-
 import {Link, Stack} from 'expo-router';
-import {StyleSheet} from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{title: 'Oops!'}} />
-      <ThemedView style={styles.container}>
-        <ThemedText style={styles.title}>This screen doesn't exist.</ThemedText>
+      <ThemedView className="flex-1 items-center justify-center p-5">
+        <ThemedText type="title" className="mb-4">
+          Pantalla no encontrada
+        </ThemedText>
 
-        <Link href="/" style={styles.link}>
-          <ThemedText style={styles.linkText}>Go to home screen!</ThemedText>
+        <Link href="/" className="mt-4 py-4">
+          <ThemedText
+            type="link"
+            className="text-light-primary dark:text-dark-primary font-sans"
+          >
+            Volver al inicio
+          </ThemedText>
         </Link>
       </ThemedView>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
